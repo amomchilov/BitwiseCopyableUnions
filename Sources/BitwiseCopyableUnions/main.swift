723df7294f1d9fcff5cdf391962076ab3849ae49
Alexander Momchilov <alexandermomchilov@gmail.com>
2024-12-26 20:21:42 -0500

    Demo union members preventing `BitwiseCopyable`

--- Sources/BitwiseCopyableUnions/main.swift
+++ Sources/BitwiseCopyableUnions/main.swift
@@ -1,3 +1,16 @@
 import DemoCModule
 
 demo_c_module_hello_world()
+
+func printACopy<T: BitwiseCopyable>(_ value: T) {
+	print(value)
+}
+
+printACopy(demo_simple_struct())
+
+printACopy(demo_struct_with_union())
+// ❌ requires that 'demo_struct_with_union' conform to 'BitwiseCopyable'
+
+printACopy(demo_copyable_struct_with_union())
+
+printACopy(demo_struct_with_copyable_union())
--- Sources/DemoCModule/include/Bridging-Header.h
+++ Sources/DemoCModule/include/Bridging-Header.h
@@ -1,6 +1,37 @@
 #ifndef DEMO_C_MODULE
 #define DEMO_C_MODULE
 
+#include <stdbool.h>
+
 void demo_c_module_hello_world();
+
+union demo_union {
+	char c;
+};
+
+union demo_copyable_union {
+	char c;
+} __attribute__((__swift_attr__("BitwiseCopyable")));
+
+// Imports as implicitly BitwiseCopyable
+struct demo_simple_struct {
+	int i;
+};
+
+// Imports as *not* BitwiseCopyable because of the union member
+struct demo_struct_with_union {
+	union demo_union u;
+};
+
+// Imports as explicitly BitwiseCopyable
+struct demo_copyable_struct_with_union {
+	union demo_union u;
+	int ints[]; // Compiler crashes without this.
+} __attribute__((__swift_attr__("BitwiseCopyable")));
+
+// Imports as implicitly BitwiseCopyable because the union is explicitly marked `BitwiseCopyable`.
+struct demo_struct_with_copyable_union {
+	union demo_copyable_union u;
+};
 
 #endif
