7e165050c92b8f02758e1553088e9537a4ed6229
Alexander Momchilov <alexandermomchilov@gmail.com>
2024-12-26 21:11:26 -0500

    Demo compiler crasher
    https://github.com/swiftlang/swift/issues/78368

--- Sources/BitwiseCopyableUnions/main.swift
+++ Sources/BitwiseCopyableUnions/main.swift
@@ -1,3 +1,12 @@
 import DemoCModule
 
-demo_c_module_hello_world()
+let crasher = demo_c_struct()
+
+func doesntCrash1(_ takeItAsAParam: demo_c_struct) {}
+func doesntCrash2() -> demo_c_struct {
+	fatalError("not implemented")
+}
+func doesntCrash3() {
+	// pass it as an argument
+	doesntCrash1(doesntCrash2())
+}
--- Sources/DemoCModule/include/Bridging-Header.h
+++ Sources/DemoCModule/include/Bridging-Header.h
@@ -3,4 +3,9 @@
 
 void demo_c_module_hello_world();
 
+struct demo_c_struct {
+	int i;
+//	int ints[]; // Compiler crashes without this.
+} __attribute__((__swift_attr__("BitwiseCopyable")));
+
 #endif
